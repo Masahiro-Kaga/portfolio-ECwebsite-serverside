4eeeea5dc233e0fdf5c15065ea318a383c52fe19
MasahiroKaga <masahiro41802891@gmail.com>
2022-02-03 19:45:36 -0800

    include initial commit Heroku

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const port = 4000;
+const port = process.env.PORT || 4000;
 const app = express();
 const cors = require("cors");
 
-app.use(cors());
 
 mongoose.connect("mongodb+srv://masahirokaga:masaMongo@cluster0.mx6qv.mongodb.net/capstone2?retryWrites=true&w=majority",{
     useNewUrlParser:true,
@@ -15,6 +14,7 @@ let db = mongoose.connection;
 db.on("error",console.error.bind(console,"Connection Error"));
 db.once("open",()=>console.log("Connected to MongoDB"));
 
+app.use(cors());
 app.use(express.json());
 
 const userRoutes = require('./routes/userRoutes');
---
+++ readme.md
@@ -0,0 +1,24 @@
+Project Name: Masahiro Store E-Commerce API
+
+Admin Credentials:
+
+email: "admin@gmail.com"
+password: "admin123"
+
+Regular User Credentials:
+
+email: "sampleUser@gmail.com"
+password: "sample123"
+
+Features:
+User registration
+User authentication
+Set user as admin (Admin only)
+Create Product (Admin only)
+Retrieve all active products
+Retrieve single product
+Update Product information (Admin only)
+Archive Product (Admin only)
+Non-admin User checkout (Create Order)
+Retrieve authenticated user’s orders
+Retrieve all orders (Admin only)
